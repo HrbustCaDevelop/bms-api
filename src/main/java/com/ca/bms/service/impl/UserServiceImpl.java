ad703b5e883e2392e065a34b3ffe0ed8f970626e
zhilong.liu <liuzhilong0627@gmail.com>
2014-11-29 14:02:56 +0800

    添加密码的加密存储

--- src/main/java/com/ca/bms/service/impl/UserServiceImpl.java
+++ src/main/java/com/ca/bms/service/impl/UserServiceImpl.java
@@ -1,27 +1,31 @@
 package com.ca.bms.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ca.bms.dao.UserDao;
-import com.ca.bms.entitys.User;
+import com.ca.bms.entitys.UserEntity;
 import com.ca.bms.enumtype.UserStatusEnum;
 import com.ca.bms.service.UserService;
+import com.ca.bms.utils.EncodeTools;
 
 /**
  * @author：刘志龙
  * @since：2014年11月28日 下午9:09:50
  * @version:1.0
  */
+@Service(value="userService")
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserDao userDao;
 	
-	public UserStatusEnum register(User user) {
+	public UserStatusEnum register(UserEntity user) {
 		if (userDao.getUserByUsername(user.getUsername()) != null) {
 			return UserStatusEnum.AIE;
 		}else {
 			try {
+				user.setPassword(EncodeTools.encoder(user.getPassword()));
 				userDao.saveUserByUser(user);
 				return UserStatusEnum.RS;
 			} catch (Exception e) {
