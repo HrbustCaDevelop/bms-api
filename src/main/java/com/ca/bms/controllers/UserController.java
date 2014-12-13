f32f1f5e491d51192716672332f7cf008ea68c88
zhilong.liu <liuzhilong0627@gmail.com>
2014-12-13 18:03:38 +0800

    去掉一个多余的引用

--- src/main/java/com/ca/bms/controllers/UserController.java
+++ src/main/java/com/ca/bms/controllers/UserController.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.servlet.http.HttpSession;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
