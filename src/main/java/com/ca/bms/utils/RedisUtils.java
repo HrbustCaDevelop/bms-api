39be95bd5c3ff1d7d5d7bf1767aab546a947a27f
alexmaven <liuzhilong0627@gmail.com>
2014-12-14 16:25:02 +0800

    1. Add Password Of Redis.

--- src/main/java/com/ca/bms/utils/RedisUtils.java
+++ src/main/java/com/ca/bms/utils/RedisUtils.java
@@ -7,7 +7,7 @@ import redis.clients.jedis.JedisPoolConfig;
 /**
  * @author：刘志龙
  * @since：2014年12月13日 下午4:56:34
- * @version:1.0
+ * @version:1.1
  */
 public class RedisUtils {
 	private static JedisPool pool;
@@ -17,8 +17,18 @@ public class RedisUtils {
 		config.setMaxIdle(200);
 		config.setTestOnBorrow(true);
 		config.setTestOnReturn(true);
-
 		pool = new JedisPool(config, "222.27.196.5", 12221);
+	}
+
+	/**
+	 * 获取一个Jedis事例，顺手认证一下
+	 * 
+	 * @return
+	 */
+	private static Jedis newInstance() {
+		Jedis jedis = pool.getResource();
+		jedis.auth("Zj4xyBkgjd.");
+		return jedis;
 	}
 
 	/**
@@ -27,7 +37,7 @@ public class RedisUtils {
 	 * @param key
 	 */
 	public static void remove(String key) {
-		Jedis jedis = pool.getResource();
+		Jedis jedis = newInstance();
 		try {
 			jedis.del(key);
 		} finally {
@@ -41,7 +51,7 @@ public class RedisUtils {
 	 * @param key
 	 */
 	public static String get(String key) {
-		Jedis jedis = pool.getResource();
+		Jedis jedis = newInstance();
 		try {
 			return jedis.get(key);
 		} finally {
@@ -56,12 +66,11 @@ public class RedisUtils {
 	 * @param value
 	 */
 	public static void put(String key, String value) {
-		Jedis jedis = pool.getResource();
+		Jedis jedis = newInstance();
 		try {
 			jedis.set(key, value);
 		} finally {
 			pool.returnResource(jedis);
 		}
-
 	}
 }
